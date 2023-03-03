a6b8a3aca3c09ea8538fcc57da276d669cd878fa
AmenAbouhamou <amen.abouhamou18@gmail.com>
2023-03-03 15:03:03 +0100

    Update login.php

--- controller/login.php
+++ controller/login.php
@@ -1,33 +1,34 @@
 <?php
 require_once '../model/sql-request.php';
 session_start();
-if(Login()){
-    Redirect('../vue/index.php?p=home', 200);
-}
-else
-    Redirect('../vue/index.php?p=login', 200);
-    function Redirect($url, $status)
-    {
-        header('Location: ' . $url, true, $status);
-        exit();
-    }
+if (Login()) {
+    echo "<script>alert('mot passe ou username error');</script>";
+} else
+    Redirect('../', 200);
 
-function Login():int{
-$userlogin=$_POST['username'];
-$passlogin=hash('sha256', $_POST['password']);
-//    $userlogin='ciccio';
-//    $passlogin=hash('sha256', 'tooor');
-    $userQuery=array();
-    $userQuery=array_merge($userQuery,getUserLogin());
-    $a=array();
-    for ($i=0;$i<count($userQuery);$i++){
-        $a=array_merge($a,$userQuery[$i]);
-        $id=$a["id"];
-        $username=$a["username"];
-        $password=$a["password"];
-        if($username==$userlogin && $passlogin==$password)
-            return $id;
+function Redirect($url, $status)
+{
+    header('Location: ' . $url, true, $status);
+    exit();
+}
+
+function Login(): bool
+{
+    $userlogin = $_POST['username'];
+    $passlogin = hash('sha256', $_POST['password']);
+    //    $userlogin='ciccio';
+    //    $passlogin=hash('sha256', 'tooor');
+    $userQuery = array();
+    $userQuery = array_merge($userQuery, getUserLogin());
+    $a = array();
+    for ($i = 0; $i < count($userQuery); $i++) {
+        $a = array_merge($a, $userQuery[$i]);
+        $id = $a["id"];
+        $username = $a["username"];
+        $password = $a["password"];
+        if ($username == $userlogin && $passlogin == $password)
+            return true;
     }
-//    print_r($userQuery);
-    return -1;
+    //    print_r($userQuery);
+    return false;
 }
