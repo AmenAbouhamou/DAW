ee9797544318c90682f652ad0ec8fabc12b2eae3
AmenAbouhamou <amen.abouhamou18@gmail.com>
2023-03-03 15:10:37 +0100

    Update login.php

---
+++ vue/pages/login.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form method="post" action="../controller/login.php">
+    <input type="text" id="username" name="username"><br>
+    <input type="password" id="password" name="password">
+    <input type="submit" value="Send">
+</form>
+</body>
+</html>
